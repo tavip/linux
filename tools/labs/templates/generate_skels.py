e45eece6a7f76d7357f2c2501a41cd5d843a819c
Octavian Purdila <tavi@cs.pub.ro>
2017-05-25 18:06:13 +0300

    tools: labs: templates: generate_skels.py: fix todo handling
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/labs/templates/generate_skels.py
+++ tools/labs/templates/generate_skels.py
@@ -5,8 +5,7 @@ import argparse, glob, os.path, re, sys
 parser = argparse.ArgumentParser(description='Generate skeletons sources from full sources')
 parser.add_argument('dirs', metavar='dir', nargs='+', help='list of directories to process')
 parser.add_argument('output', help='output dir to copy processed files')
-parser.add_argument('--todo', type=int, help='don\'t remove TODOs less then this', default=0)
-parser.add_argument('--skip-path', type=int, help='skip path', default=0)
+parser.add_argument('--todo', type=int, help='don\'t remove TODOs less then this', default=1)
 args = parser.parse_args()
 
 for d in args.dirs:
@@ -18,7 +17,7 @@ for d in args.dirs:
 		except:
 			pass
 		if name == "Kbuild":
-			pattern="(#\s*TODO)([0-9]*)\/([0-9]*)(.*)"
+			pattern="(#\s*TODO)([0-9]*)\/?([0-9]*)(.*)"
 		else:
 			pattern="(\s*/\*\s*TODO)([0-9]*)/?([0-9]*)(.*)"
 		f = open(p)
@@ -30,12 +29,14 @@ for d in args.dirs:
 				continue
 			m = re.search(pattern, l)
 			if m:
-				todo=0
-				skip_lines=1
+				todo=1
 				if m.group(2):
 					todo = m.group(2)
-				if m.group(3) and todo >= args.todo:
-					skip_lines = int(m.group(3))
+				if todo >= args.todo:
+					if m.group(3):
+						skip_lines = int(m.group(3))
+					else:
+						skip_lines = 1
 				l = "%s%s%s\n" % (m.group(1), m.group(2), m.group(4))
 			g.write(l)
 
