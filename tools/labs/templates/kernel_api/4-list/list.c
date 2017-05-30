bdbdaf4d63e6450082bc5f8639bd770cbf84cf95
Daniel Baluta <daniel.baluta@nxp.com>
2017-05-30 18:20:43 +0300

    tools: labs: Fix kernel_api templates
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- tools/labs/templates/kernel_api/4-list/list.c
+++ tools/labs/templates/kernel_api/4-list/list.c
@@ -60,7 +60,7 @@ static void task_info_print_list(const char *msg)
 	struct list_head *p;
 	struct task_info *ti;
 
-	pr_info(LOG_LEVEL "%s: [ ", msg);
+	pr_info("%s: [ ", msg);
 	list_for_each(p, &head) {
 		ti = list_entry(p, struct task_info, list);
 		pr_info("(%d, %lu) ", ti->pid, ti->timestamp);
